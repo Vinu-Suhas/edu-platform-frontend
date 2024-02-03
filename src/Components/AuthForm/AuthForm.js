2e8cf28cd601c59d1539ae56a8a182ea480a36e3
Vinu Suhas <vinusuhas2000@gmail.com>
2024-02-03 18:57:01 +0530

    updated

--- src/Components/AuthForm/AuthForm.js
+++ src/Components/AuthForm/AuthForm.js
@@ -1,7 +1,44 @@
-import React from 'react'
-
+import React, { useState } from "react";
+import "./AuthForm.css";
 export const AuthForm = () => {
+  const [authState, setAuthState] = useState("");
   return (
-    <div>AuthForm</div>
-  )
-}
+    <div className="authContainer">
+      <div className="authContainerLeft">
+        <img src="https://s3.ap-south-1.amazonaws.com/www.prepbytes.com/images/illustrated.webp" />
+        <div className="prepbytesAchievementsContainer">
+          <img src="https://s3.ap-south-1.amazonaws.com/www.prepbytes.com/images/logo.webp"  className="prepbytesIcon"/>
+          <div className="prepbytesAchievementsChild">
+            <img src="https://s3.ap-south-1.amazonaws.com/www.prepbytes.com/images/students.webp" />
+            <div className="prepbytesAchievementsChildDetails">
+              <p className="prepbytesAchievementsChildCount">10K+</p>
+              <p>Studetnts Enrolled</p>
+            </div>
+          </div>
+          <div className="prepbytesAchievementsChild">
+            <img src="https://s3.ap-south-1.amazonaws.com/www.prepbytes.com/images/college.webp" />
+            <div className="prepbytesAchievementsChildDetails">
+              <p className="prepbytesAchievementsChildCount">1000+</p>
+              <p>Reach in Colleges</p>
+            </div>
+          </div>
+          <div className="prepbytesAchievementsChild">
+            <img src="https://s3.ap-south-1.amazonaws.com/www.prepbytes.com/images/learning.webp" />
+            <div className="prepbytesAchievementsChildDetails">
+              <p className="prepbytesAchievementsChildCount">1000+</p>
+              <p>Reach in Colleges</p>
+            </div>
+          </div>
+          <div className="prepbytesAchievementsChild">
+            <img src="https://s3.ap-south-1.amazonaws.com/www.prepbytes.com/images/social.webp" />
+            <div className="prepbytesAchievementsChildDetails">
+              <p className="prepbytesAchievementsChildCount">150K+</p>
+              <p>Social Community</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="authContainerRight"></div>
+    </div>
+  );
+};
--- src/Components/RouterCompo/RouterCompo.js
+++ src/Components/RouterCompo/RouterCompo.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import { Home } from "../Home/Home";
+import { AuthForm } from "../AuthForm/AuthForm";
 
 export const RouterCompo = () => {
   return (
     <Routes>
       <Route path="/home" element={<Home />} />
+      <Route path="/login" element={<AuthForm />} />
     </Routes>
   );
 };
