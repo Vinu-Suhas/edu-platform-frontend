b2f06ea5b9e2550c879c8bba8e496d0c74409008
Vinu Suhas <vinusuhas2000@gmail.com>
2024-02-06 21:55:04 +0530

    updated:

--- src/Components/AuthForm/AuthForm.js
+++ src/Components/AuthForm/AuthForm.js
@@ -1,13 +1,16 @@
 import React, { useState } from "react";
 import "./AuthForm.css";
-export const AuthForm = () => {
-  const [authState, setAuthState] = useState("");
+export const AuthForm = (props) => {
+  const [authState, setAuthState] = useState("Login");
   return (
     <div className="authContainer">
       <div className="authContainerLeft">
         <img src="https://s3.ap-south-1.amazonaws.com/www.prepbytes.com/images/illustrated.webp" />
         <div className="prepbytesAchievementsContainer">
-          <img src="https://s3.ap-south-1.amazonaws.com/www.prepbytes.com/images/logo.webp"  className="prepbytesIcon"/>
+          <img
+            src="https://s3.ap-south-1.amazonaws.com/www.prepbytes.com/images/logo.webp"
+            className="prepbytesIcon"
+          />
           <div className="prepbytesAchievementsChild">
             <img src="https://s3.ap-south-1.amazonaws.com/www.prepbytes.com/images/students.webp" />
             <div className="prepbytesAchievementsChildDetails">
@@ -38,7 +41,25 @@ export const AuthForm = () => {
           </div>
         </div>
       </div>
-      <div className="authContainerRight"></div>
+      <div className="authContainerRight">
+        <div className="authFormInput">
+          <div className="authHeading">
+            <p onClick={() => setAuthState("Register")}>SIGN UP</p>
+            <p onClick={() => setAuthState("Login")}>LOGIN</p>
+          </div>
+          <hr style={{ color: "black" }} />
+          {authState === "Login" ? (
+            <p className="authTitle">Sign in to your account</p>
+          ) : (
+            <p className="authTitle"> Create your new account</p>
+          )}
+          <input type="email" />
+          <input type="password" />
+          <p>Forgot your password?</p>
+          <button className="authButtons">SIGN IN</button>
+          <button className="authButtons">LOGIN VIA OTP</button>
+        </div>
+      </div>
     </div>
   );
 };
