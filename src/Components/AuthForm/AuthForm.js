86803e9ab5b2b9717dc571c1b019be501f835db8
Vinu Suhas <vinusuhas2000@gmail.com>
2024-02-07 23:57:25 +0530

    updated

--- src/Components/AuthForm/AuthForm.js
+++ src/Components/AuthForm/AuthForm.js
@@ -53,10 +53,16 @@ export const AuthForm = (props) => {
           ) : (
             <p className="authTitle"> Create your new account</p>
           )}
-          <input type="email" />
-          <input type="password" />
-          <p>Forgot your password?</p>
-          <button className="authButtons">SIGN IN</button>
+          <h4>Email</h4>
+          <input type="email" name="email" />
+          <h4>Password</h4>
+          <input type="password" name="password" />
+          <h4>Phone no</h4>
+          <input type="number" name="pnumber" />
+          {authState === "Login" ? <p>Forgot your password?</p> : null}
+          <button className="authButtons" style={{ marginBottom: "10px" }}>
+            SIGN IN
+          </button>
           <button className="authButtons">LOGIN VIA OTP</button>
         </div>
       </div>
