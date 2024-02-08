89c7edf7f01e7b4af8c86107fa7a7f51856774d2
Vinu Suhas <vinusuhas2000@gmail.com>
2024-02-09 00:08:24 +0530

    Feat:updated

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import "./App.css";
 import { Header } from "./Components/Header/Header";
 import { BrowserRouter } from "react-router-dom";
 import { RouterCompo } from "./Components/RouterCompo/RouterCompo";
+import { Footer } from "./Components/Footer/Footer";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Header />
-      <RouterCompo />
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <Header />
+        <RouterCompo />
+      </BrowserRouter>
+      <Footer />
+    </>
   );
 }
 
---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,47 @@
+import React from "react";
+import "./Footer.css";
+export const Footer = () => {
+  return (
+    <footer className="footerContainer">
+      <div className="footerRow1">
+        <div className="footerColumn1">
+          <p>FOLLOW US</p>
+          <p> CONTACT US</p>
+          <p>+91-7969 0021 11</p>
+          <p>support@prepbytes.com</p>
+        </div>
+        <div className="footerColumn2">
+          <p>LATEST ARTICLES</p>
+          <p>sed Command in Linux Unix With</p>
+          <p>Examples</p>
+          <p>scp Command in Linux with Examples</p>
+          <p>chown Command in Linux with </p>
+          <p>Examples</p>
+          <p>cp Command Linux Examples</p>
+          <p>Touch Command in Linux with </p>
+          <p>Examples</p>
+          <p>POPULAR ARTICLES</p>
+          <p>dd command linux</p>
+          <p>ifconfig Command in Linux</p>
+          <p>with Examples</p>
+          <p>pwd Command in Linux with</p>
+          <p>Examples</p>
+          <p>diff Command Linux</p>
+          <p>Examples</p>
+          <p>Top Commandn in Linux with</p>
+          <p>Examples</p>
+        </div>
+        <div className="footerColumn3">
+          <p>QUICK LINKS</p>
+          <p> Interview Notes</p>
+          <p>Mock Test</p>
+          <p>Placement</p>
+          <p>Programs</p>
+          <p> Coding Courses </p>
+          <p> About Us</p>
+          <p>Blog</p>
+        </div>
+      </div>
+    </footer>
+  );
+};
