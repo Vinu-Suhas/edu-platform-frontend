06026aa7d6a4ce5484a88e1da16eb3ae825da183
Vinu Suhas <vinusuhas2000@gmail.com>
2024-02-12 08:12:16 +0530

    feat: updated

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -25,10 +25,33 @@ export const Home = () => {
         <img src="https://s3.ap-south-1.amazonaws.com/www.prepbytes.com/images/homepage/stairs_of_approach(web).svg" />
       </div>
       <div className="homecontainer4">
-        <h1>I WANT TO</h1>
-        <img src="https://s3.ap-south-1.amazonaws.com/www.prepbytes.com/images/homepage/bg_master_competetive.webp" />
-        <img src="https://s3.ap-south-1.amazonaws.com/www.prepbytes.com/images/homepage/bg_master_competetive.webp" />
-        <img src="https://s3.ap-south-1.amazonaws.com/www.prepbytes.com/images/homepage/bg_master_competetive.webp" />
+        <h1 style={{ textAlign: "center" }}>I WANT TO</h1>
+        <div className="courseCardContainer">
+          <div className="courseCard">
+            <img
+              src="https://prepbytes.netlify.app/static/media/Full_Stack.a01a607d720680462d14.webp"
+              width={"100%"}
+              alt="poster"
+            />
+            <h2>PREPARE FOR CAMPUS PLACEMENTS</h2>
+          </div>
+          <div className="courseCard">
+            <img
+              src="https://prepbytes.netlify.app/static/media/master_competetive_pgm.1dbf0bfd3bb3d39d541d.webp"
+              width={"100%"}
+              alt="poster"
+            />
+            <h2>MASTER COMPETITIVE PROGRAMMING</h2>
+          </div>
+          <div className="courseCard">
+            <img
+              src="https://prepbytes.netlify.app/static/media/Project_Poster.d4fcaeb59c6fc625ac4a.webp"
+              width={"100%"}
+              alt="poster"
+            />
+            <h2>BUILD DEVELOPMENT PROJECTS</h2>
+          </div>
+        </div>
       </div>
     </>
   );
